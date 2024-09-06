ab8226e1405aafac0358ce5ce866faae74635a87
Aniruddha Ranade <18034541+AniruddhaRanade@users.noreply.github.com>
2024-09-07 00:29:46 +0530

    Update test.py

--- test/test.py
+++ test/test.py
@@ -7,7 +7,7 @@ from cocotb.triggers import ClockCycles
 
 
 @cocotb.test()
-async def test_loopback(dut):
+async def test_project(dut):
     dut._log.info("Start")
 
     # Set the clock period to 10 us (100 KHz)
@@ -16,42 +16,25 @@ async def test_loopback(dut):
 
     # Reset
     dut._log.info("Reset")
-
-    # When under reset: ui_in -> uo_out
+    dut.ena.value = 1
     dut.ui_in.value = 0
-    dut.rst_n.value = 0
-    await ClockCycles(dut.clk, 1)
-
-    for i in range(256):
-        dut.ui_in.value = i
-        await ClockCycles(dut.clk, 1)
-        assert dut.uo_out.value == i
-
-@cocotb.test()
-async def test_counter(dut):
-    dut._log.info("Start")
-
-    # Set the clock period to 10 us (100 KHz)
-    clock = Clock(dut.clk, 10, units="us")
-    cocotb.start_soon(clock.start())
-
-    # rst_n == 0: put a counter on uo_out
+    dut.uio_in.value = 0
     dut.rst_n.value = 0
     await ClockCycles(dut.clk, 10)
     dut.rst_n.value = 1
-    await ClockCycles(dut.clk, 2)
 
-    dut._log.info("Testing counter")
-    for i in range(256):
-        assert dut.uo_out.value == i
-        await ClockCycles(dut.clk, 1)
+    dut._log.info("Test project behavior")
 
-    dut._log.info("Testing reset")
-    for i in range(5):
-        assert dut.uo_out.value == i
-        await ClockCycles(dut.clk, 1)
-    dut.rst_n.value = 0
-    await ClockCycles(dut.clk, 2)
-    dut.rst_n.value = 1
+    # Set the input values you want to test
+    dut.ui_in.value = 20
+    dut.uio_in.value = 30
+
+    # Wait for one clock cycle to see the output values
     await ClockCycles(dut.clk, 1)
+
+    # The following assersion is just an example of how to check the output values.
+    # Change it to match the actual expected output of your module:
     assert dut.uo_out.value == 0
+
+    # Keep testing the module by changing the input values, waiting for
+    # one or more clock cycles, and asserting the expected output values.
